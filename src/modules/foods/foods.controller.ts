d6090d1859ab8c8191c8a1e94bdb2eb0e3d5f96c
diegui98 <diegui98@live.com.ar>
2026-03-13 17:53:42 -0300

    foods: implementado controller, nuevo DTO update-food

--- src/modules/foods/foods.controller.ts
+++ src/modules/foods/foods.controller.ts
@@ -1,34 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { FoodsService } from './foods.service';
 import { CreateFoodDto } from './dto/create-food.dto';
 import { UpdateFoodDto } from './dto/update-food.dto';
+import { Food } from './entities/food.entity';
 
 @Controller('foods')
 export class FoodsController {
   constructor(private readonly foodsService: FoodsService) {}
 
   @Post()
-  create(@Body() createFoodDto: CreateFoodDto) {
+  create(@Body() createFoodDto: CreateFoodDto): Promise<Food> {
     return this.foodsService.create(createFoodDto);
   }
 
-  @Get()
-  findAll() {
-    return this.foodsService.findAll();
+  @Get('account/:accountId')
+  findAllByAccount(
+    @Param('accountId') accountIdParam: string,
+  ): Promise<Food[]> {
+    const accountId: number = parseInt(accountIdParam, 10);
+    return this.foodsService.findAllByAccount(accountId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.foodsService.findOne(+id);
+  findOne(@Param('id') id: string): Promise<Food> {
+    return this.foodsService.findOne(Number(id));
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFoodDto: UpdateFoodDto) {
-    return this.foodsService.update(+id, updateFoodDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateFoodDto: UpdateFoodDto,
+  ): Promise<Food> {
+    return this.foodsService.update(Number(id), updateFoodDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.foodsService.remove(+id);
+  remove(@Param('id') id: string): Promise<void> {
+    return this.foodsService.remove(Number(id));
   }
 }
