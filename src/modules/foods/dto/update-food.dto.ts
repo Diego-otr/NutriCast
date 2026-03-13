1de7ad48cd18fb316d0cd5b8edc351a467ab09cc
diegui98 <diegui98@live.com.ar>
2026-03-13 18:09:11 -0300

    foods: update-food utiliza partialType

--- src/modules/foods/dto/update-food.dto.ts
+++ src/modules/foods/dto/update-food.dto.ts
@@ -1,28 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateFoodDto } from './create-food.dto';
-import {
-  IsString,
-  IsNumber,
-  IsNotEmpty,
-  IsOptional,
-  Min,
-  MaxLength,
-} from 'class-validator';
+import {} from 'class-validator';
 
-export class UpdateFoodDto extends PartialType(CreateFoodDto) {
-  @IsString()
-  @IsNotEmpty({ message: 'El nombre del alimento es obligatorio' })
-  @MaxLength(40)
-  @IsOptional() // En la actualización, el nombre también puede ser opcional
-  name: string;
-
-  @IsNumber()
-  @Min(0)
-  @IsOptional()
-  caloriesPerGram?: number;
-
-  @IsNumber()
-  @Min(0)
-  @IsOptional()
-  caloriesPerPortion?: number;
-}
+export class UpdateFoodDto extends PartialType(CreateFoodDto) {}
