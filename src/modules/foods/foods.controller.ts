e9f38ac20a7546e87041258497aa18974c0d77b8
diegui98 <diegui98@live.com.ar>
2026-04-08 23:39:04 -0300

    foods: agregado return types a controller

--- src/modules/foods/foods.controller.ts
+++ src/modules/foods/foods.controller.ts
@@ -14,6 +14,7 @@ import { UpdateFoodDto } from './dto/update-food.dto';
 import { FoodItemHateoasInterceptor } from './interceptors/food-item-hateoas.interceptor';
 import { FoodCollectionHateoasInterceptor } from './interceptors/food-collection-hateoas.interceptor';
 import { FoodDeleteHateoasInterceptor } from './interceptors/food-delete-hateoas.interceptor';
+import { Food } from './entities/food.entity';
 
 @Controller('foods')
 export class FoodsController {
@@ -21,32 +22,37 @@ export class FoodsController {
 
   @Post()
   @UseInterceptors(FoodItemHateoasInterceptor) // <--- Interceptor Individual
-  async create(@Body() createFoodDto: CreateFoodDto) {
+  async create(@Body() createFoodDto: CreateFoodDto): Promise<Food> {
     return this.foodsService.create(createFoodDto);
   }
 
   @Get('account/:accountId')
   @UseInterceptors(FoodCollectionHateoasInterceptor) // <--- Interceptor de Colección
-  async findAllByAccount(@Param('accountId') accountIdParam: string) {
+  async findAllByAccount(
+    @Param('accountId') accountIdParam: string,
+  ): Promise<Food[]> {
     const accountId: number = parseInt(accountIdParam, 10);
     return this.foodsService.findAllByAccount(accountId);
   }
 
   @Get(':id')
   @UseInterceptors(FoodItemHateoasInterceptor) // <--- Interceptor Individual
-  async findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string): Promise<Food> {
     return this.foodsService.findOne(Number(id));
   }
 
   @Patch(':id')
   @UseInterceptors(FoodItemHateoasInterceptor) // <--- Interceptor Individual
-  async update(@Param('id') id: string, @Body() updateFoodDto: UpdateFoodDto) {
+  async update(
+    @Param('id') id: string,
+    @Body() updateFoodDto: UpdateFoodDto,
+  ): Promise<Food> {
     return this.foodsService.update(Number(id), updateFoodDto);
   }
 
   @Delete(':id')
   @UseInterceptors(FoodDeleteHateoasInterceptor) // <--- Interceptor de Eliminación
-  async remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string): Promise<{ message: string }> {
     await this.foodsService.remove(Number(id));
     return {
       message: `Alimento con ID ${id} eliminado correctamente`,
